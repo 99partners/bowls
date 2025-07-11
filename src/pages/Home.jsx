d2baa050cb86fe36f3256113ed924daede0eeb78
rangpariya <utsavrangpariya62440@gmail.com>
2025-07-11 10:15:52 +0530

    bowl

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -176,7 +176,7 @@ const Index = () => {
           }
 
           .animate-gentle-rotate {
-            animation: gentle-rotate 40s linear infinite;
+            animation: gentle-rotate 100s linear infinite;
           }
         `}</style>
       </section>
