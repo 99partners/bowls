51ceb363b0233431a0339f147f64f36851e21d02
Anuj Patel <patel.anujp1406@gmail.com>
2025-07-30 14:40:39 +0530

    admin bowls

--- client/src/components/admin/components/Sidebar.jsx
+++ client/src/components/admin/components/Sidebar.jsx
@@ -5,6 +5,7 @@ import "../styles/main.css";
 const Sidebar = () => {
   return (
     <aside className="admin-sidebar">
+      <h1 className="sidebar-title">99 Bowls</h1>
       <h2>Admin Panel</h2>
       <nav>
         <ul>
