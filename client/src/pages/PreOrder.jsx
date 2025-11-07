16031e4b43857853f6449a31cc6e32edb0d51021
rangpariya <utsavrangpariya62440@gmail.com>
2025-11-07 12:21:07 +0530

    preorder reel

--- client/src/pages/PreOrder.jsx
+++ client/src/pages/PreOrder.jsx
@@ -788,7 +788,7 @@ const PreOrder = () => {
               </div>
 
               {/* Video Reels Section */}
-              <div className="mb-12 overflow-hidden">
+              {/* <div className="mb-12 overflow-hidden">
                 <h3 className="text-xl font-bold text-orange-500 mb-4">
                   Video Reels
                 </h3>
@@ -845,7 +845,7 @@ const PreOrder = () => {
                     ))}
                   </div>
                 </div>
-              </div>
+              </div> */}
 
               <style>
                 {`
